749bf20570ebe1afdcd0872e9b371c519fd97305
panshijia1996 <932955926@qq.com>
2020-09-02 15:56:31 +0800

    feat: 热更新补充学习

--- src/main.js
+++ src/main.js
@@ -5,11 +5,20 @@
 import $ from 'jquery'
 
 $(function(){
-    $('li:odd').css('backgroundColor','lightblue')
+    $('li:odd').css('backgroundColor','blue')
     $('li:even').css('backgroundColor',function(){
         return '#' + 'D97634'
     })
 })
 
 // 使用webpack-dev-server这个工具，来实现自动打包编译的功能
-// 1、运行 npm i webpack-dev-server -D 把这个工具安装到项目的本地开发依赖
+// 1、运行 npm i webpack-dev-server -D 把这个工具安装到项目的本地开发依赖，
+// 但是他是在本地安装的，所以不能直接当做脚本命令直接在终端运行，只有全局安装的工具才能在终端中正常执行
+// 因此可以在package.json中配置要执行的命令
+// 并且要注意的是，webpack-dev-server要求在本地项目中也安装webpack，要本地装一下
+// 运行之后，可以从Project is running at http://localhost:8081/ 进入到项目的根路径
+// 打包好的bundle.js想要访问的话，直接通过根路径http://localhost:8081/bundle.js就能访问
+// index.html页面中我们引入的bundle.js是dist文件夹中的，但是webpack-dev-server自动打包生成的bundle.js是在根目录下的
+// 这两个不是一样的，所以我们修改代码保存后，页面上并不会更改，因为改的是根目录下的bundle.js、
+// 所以我们要引入根目录下的bundle.js，这个打包生成的bundle.js文件并没有存放到实际的物理磁盘上，而是直接托管到了内存中
+// 所以在项目根目录中根本找不到这个打包好的bundle.js，放到内存中的好处就是读取速度快
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+// 启用热更新的第二步
+const webpack = require('webpack')
 // 这个配置文件是一个js文件，通过node中的模块操作，向外暴露了一个配置对象
 module.exports = {
     // 入口，表示要使用webpack打包那个文件
@@ -8,5 +10,14 @@ module.exports = {
         path:path.join(__dirname, './dist'), // 指定打包好的文件要输出到那个目录中去
         filename: 'bundle.js' // 这是指定输出的文件的名称
     },
+    devServer: { // 这是配置dev-server命令参数的第二种形式
+        open: true, // 自动打开浏览器
+        // prot: 3000, // 设置启动时候的运行端口
+        contentBase: 'src', // 指定托管的根目录
+        hot: true // 启用热更新的第一步
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin() // new了一个热更新的模块对象，启用热更新的第三步
+    ],
     mode: 'development'
 }
